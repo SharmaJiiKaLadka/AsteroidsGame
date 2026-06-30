60f60f182f56e77a1fb771401c5cca524280a4ba
KshitijSh-2604 <kshitij466e@gmail.com>
2026-06-30 23:44:51 +0530

    B: Till Groups

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 import pygame
+from player import Player
 from constants import *
 from logger import log_state
 
 pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+clock = pygame.time.Clock()
+dt = 0.0
+
+player = Player(
+    SCREEN_WIDTH / 2,
+    SCREEN_HEIGHT / 2,
+)
 
 while True:
     log_state()
@@ -11,8 +20,12 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+    player.update(dt)
     screen.fill("black")
+    player.draw(screen)
     pygame.display.flip()
+    dt = clock.tick(60) / 1000.0
+
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
